268bbb0b97390f4d88b1e3e52c7fb76288f31252
Jay Lauffer <jv8yves@proton.me>
2026-08-08 13:13:29 +0700

    Fixes to break from wait state.

--- proactor/src/uring.rs
+++ proactor/src/uring.rs
@@ -14,6 +14,7 @@ const MAX_EVENTS: usize = 256;
 pub struct IoUringPort {
     ring: Mutex<IoUring>,
     queue: Mutex<VecDeque<CompletionEnvelope>>,
+    wake_fd: RawFd,  // eventfd for waking
 }
 
 impl IoUringPort {
@@ -21,9 +22,15 @@ impl IoUringPort {
         let ring = IoUring::new(MAX_EVENTS as u32)
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
 
+        let wake_fd = unsafe { libc::eventfd(0, libc::EFD_CLOEXEC | libc::EFD_NONBLOCK) };
+        if wake_fd == -1 {
+            return Err(io::Error::last_os_error());
+        }
+
         Ok(Self {
             ring: Mutex::new(ring),
             queue: Mutex::new(VecDeque::new()),
+            wake_fd,
         })
     }
 
@@ -61,6 +68,38 @@ impl IoUringPort {
 
         Ok(())
     }
+
+        fn signal_eventfd(fd: RawFd) -> io::Result<()> {
+        let value: u64 = 1;
+        let rc = unsafe {
+            libc::write(fd, &value as *const u64 as *const libc::c_void, std::mem::size_of::<u64>())
+        };
+        if rc == -1 {
+            Err(io::Error::last_os_error())
+        } else {
+            Ok(())
+        }
+    }
+
+    fn clear_eventfd(fd: RawFd) -> io::Result<()> {
+        loop {
+            let mut value = 0u64;
+            let rc = unsafe {
+                libc::read(fd, &mut value as *mut u64 as *mut libc::c_void, std::mem::size_of::<u64>())
+            };
+            if rc == -1 {
+                let err = io::Error::last_os_error();
+                match err.raw_os_error() {
+                    Some(libc::EAGAIN) => return Ok(()),
+                    Some(libc::EINTR) => continue,
+                    _ => return Err(err),
+                }
+            }
+            if rc == 0 {
+                return Err(io::Error::from_raw_os_error(libc::EINVAL));
+            }
+        }
+    }
 }
 
 impl CompletionPort for IoUringPort {
@@ -75,7 +114,6 @@ impl CompletionPort for IoUringPort {
     }
 
     fn poll(&self, timeout: Option<Duration>) -> io::Result<PollEvent> {
-        // Check queue first (non-blocking)
         if let Some(envelope) = self.drain_completion() {
             return Ok(PollEvent::Completion(envelope));
         }
@@ -84,7 +122,22 @@ impl CompletionPort for IoUringPort {
             io::Error::new(io::ErrorKind::Other, format!("lock poisoned: {}", e))
         )?;
 
-        // Wait for completion events
+        // Register eventfd for poll if not already registered
+        // We need to submit a POLL_ADD for wake_fd
+        let poll_op = opcode::PollAdd::new(types::Fd(self.wake_fd), libc::POLLIN as u32)
+            .build()
+            .user_data(WAKE_TOKEN);
+
+        unsafe {
+            ring.submission()
+                .push(&poll_op)
+                .map_err(|_| io::Error::new(io::ErrorKind::Other, "submission queue full"))?;
+        }
+
+        ring.submit()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+
+        // Wait for events
         let result = if let Some(duration) = timeout {
             let ts = Self::duration_to_timespec(duration);
             let uring_ts = types::Timespec::new()
@@ -104,7 +157,7 @@ impl CompletionPort for IoUringPort {
             Err(e) => return Err(e),
         }
 
-        // Process completion queue entries
+        // Process completion queue
         let mut cq = ring.completion();
         if let Some(cqe) = cq.next() {
             match cqe.user_data() {
@@ -115,7 +168,10 @@ impl CompletionPort for IoUringPort {
                         Ok(PollEvent::Wake)
                     }
                 }
-                WAKE_TOKEN => Ok(PollEvent::Wake),
+                WAKE_TOKEN => {
+                    Self::clear_eventfd(self.wake_fd)?;
+                    Ok(PollEvent::Wake)
+                }
                 _ => Ok(PollEvent::Wake),
             }
         } else {
@@ -124,6 +180,6 @@ impl CompletionPort for IoUringPort {
     }
 
     fn wake(&self) -> io::Result<()> {
-        self.signal_wake(WAKE_TOKEN)
+        Self::signal_eventfd(self.wake_fd)
     }
 }
